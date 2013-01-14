189ea0234399df30df51650dc1fbe3bd9e38cae7
Skylar Schipper <ss@schipp.co>
2013-01-14 09:34:12 -0800

    Disable sertificate chain checking.

--- SocketRocket/SRWebSocket.m
+++ SocketRocket/SRWebSocket.m
@@ -592,10 +592,10 @@ static __strong NSData *CRLFCRLF;
             [SSLOptions setValue:[NSNumber numberWithBool:NO] forKey:(__bridge id)kCFStreamSSLValidatesCertificateChain];
         }
         
-#if DEBUG
+
         [SSLOptions setValue:[NSNumber numberWithBool:NO] forKey:(__bridge id)kCFStreamSSLValidatesCertificateChain];
         NSLog(@"SocketRocket: In debug mode.  Allowing connection to any root cert");
-#endif
+
         
         [_outputStream setProperty:SSLOptions
                             forKey:(__bridge id)kCFStreamPropertySSLSettings];
