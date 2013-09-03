16b16b3952988136df9f22277687dd50701b82b4
Skylar Schipper <ss@schipp.co>
2013-09-03 10:49:21 -0700

    Fixed warnings

--- SocketRocket/SRWebSocket.m
+++ SocketRocket/SRWebSocket.m
@@ -665,12 +665,9 @@ static __strong NSData *CRLFCRLF;
             NSRange remainingRange = {0};
             
             NSUInteger usedLength = 0;
-            
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunused-variable"
+
             BOOL success = [reason getBytes:(char *)mutablePayload.mutableBytes + sizeof(uint16_t) maxLength:payload.length - sizeof(uint16_t) usedLength:&usedLength encoding:NSUTF8StringEncoding options:NSStringEncodingConversionExternalRepresentation range:NSMakeRange(0, reason.length) remainingRange:&remainingRange];
-#pragma clang diagnostic pop
-            
+#pragma unused(success)
             
             assert(success);
             assert(remainingRange.length == 0);
@@ -1034,10 +1031,8 @@ static const uint8_t SRPayloadLenMask   = 0x7F;
             [self _handleFrameHeader:header curData:self->_currentFrameData];
         } else {
             [self _addConsumerWithDataLength:extra_bytes_needed callback:^(SRWebSocket *self, NSData *data) {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunused-variable"
                 size_t mapped_size = data.length;
-#pragma clang diagnostic pop
+#pragma unused(mapped_size)
                 const void *mapped_buffer = data.bytes;
                 size_t offset = 0;
                 
